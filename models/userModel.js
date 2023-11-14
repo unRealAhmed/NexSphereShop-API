78635d7db1d28352925333c166879f3b93c55ac5
Ahmed Sayed <therealahme@gmail.com>
2023-11-14 04:49:00 +0200

    create user model

---
+++ models/userModel.js
@@ -0,0 +1,71 @@
+const mongoose = require('mongoose')
+
+const userShema = new mongoose.Schema(
+  {
+    fullname: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    orders: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Order",
+      },
+    ],
+    wishLists: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "WishList",
+      },
+    ],
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    hasShippingAddress: {
+      type: Boolean,
+      default: false,
+    },
+    shippingAddress: {
+      firstName: {
+        type: String,
+      },
+      lastName: {
+        type: String,
+      },
+      address: {
+        type: String,
+      },
+      city: {
+        type: String,
+      },
+      postalCode: {
+        type: String,
+      },
+      province: {
+        type: String,
+      },
+      country: {
+        type: String,
+      },
+      phone: {
+        type: String,
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model('User', userShema)
+
+module.exports = User
