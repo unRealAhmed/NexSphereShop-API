0f9c81656bd3998e9c14f3ce50f0ffffd46c2a95
Ahmed Sayed <therealahme@gmail.com>
2023-11-15 04:41:57 +0200

    add missing routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,54 +1,48 @@
 const express = require('express');
-const authController = require('../controllers/authController');
-// const userController = require('../controllers/userController');
+const {
+  getMe,
+  getUser,
+  updateMe,
+  deleteMe,
+  getAllUsers,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require('../controllers/userController');
 
 const {
   signup,
   login,
+  logout,
   forgetPassword,
   resetPassword,
-  updatePassword,
   protect,
-  // restrictTo,
-} = authController;
-
-// const {
-//   getMe,
-//   getUser,
-//   getAllUsers,
-//   createUser,
-//   updateUser,
-//   deleteUser,
-//   updateMe,
-//   deleteMe,
-// } = userController;
+  restrictTo,
+  updatePassword,
+} = require('../controllers/authController');
 
 const router = express.Router();
-
+// Authentication routes
 router.post('/signup', signup);
 router.post('/login', login);
+router.get('/logout', logout);
 router.post('/forgotPassword', forgetPassword);
 router.patch('/resetPassword/:token', resetPassword);
 
 // Protect all routes after this middleware
 router.use(protect);
 
+// User routes
+router.get('/me', getMe, getUser);
 router.patch('/updateMyPassword', updatePassword);
-// router.get('/me', getMe, getUser);
-// router.patch('/updateMe', updateMe);
-// router.delete('/deleteMe', deleteMe);
+router.patch('/updateMe', updateMe);
+router.delete('/deleteMe', deleteMe);
 
-// router.use(restrictTo('admin'));
+// Admin-restricted routes
+router.use(restrictTo('admin'));
 
-// router
-//   .route('/')
-//   .get(getAllUsers)
-//   .post(createUser);
-
-// router
-//   .route('/:id')
-//   .get(getUser)
-//   .patch(updateUser)
-//   .delete(deleteUser);
+// User management routes
+router.route('/').get(getAllUsers).post(createUser);
+router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
 module.exports = router;
