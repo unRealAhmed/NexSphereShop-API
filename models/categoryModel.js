5a0a14a2ad4f1b92818def819645902e4d856390
Ahmed Sayed <therealahme@gmail.com>
2023-11-15 11:49:23 +0200

    finish category model

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -13,7 +13,7 @@ const categorySchema = new mongoose.Schema(
     },
     image: {
       type: String,
-      required: [true, 'Please provide an image for the category.'],
+      // required: [true, 'Please provide an image for the category.'],
     },
     products: [
       {
