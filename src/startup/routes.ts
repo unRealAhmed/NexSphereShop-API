fd0fdf7b6f96eee77d14e0dcde713e86cddcda05
Ahmed Sayed <ahmed.sayed.connect@gmail.com>
2024-03-08 14:10:47 +0200

    add color routes

--- src/startup/routes.ts
+++ src/startup/routes.ts
@@ -3,6 +3,7 @@ import swaggerDocs from '../config/swagger.config'
 import auth from '../modules/auth/auth.routes'
 import brand from '../modules/brand/brand.routes'
 import category from '../modules/category/category.routes'
+import color from '../modules/color/color.routes'
 import healthCheck from '../modules/healthCheck'
 import product from '../modules/product/product.routes'
 import user from '../modules/user/user.routes'
@@ -14,6 +15,7 @@ export default function (app: Express, port: number) {
     app.use('/api/products', product)
     app.use('/api/categories', category)
     app.use('/api/brands', brand)
+    app.use('/api/colors', color)
 
     swaggerDocs(app, port)
 }
