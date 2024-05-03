cab89163651acef2ec16b24e743bc0c9fa1754f8
Ahmed Sayed <ahmed.sayed.connect@gmail.com>
2024-05-03 16:42:07 +0300

    add method for soft delete

--- src/repositories/brand.repository.ts
+++ src/repositories/brand.repository.ts
@@ -1,8 +1,15 @@
 import Brand, { IBrand } from '../models/brand.model'
+import { ID } from '../shared/types'
 import { AbstractRepository } from './abstract.repository'
 
 export class BrandRepository extends AbstractRepository<IBrand> {
     constructor() {
         super(Brand)
+    }
+
+    softDelete(id: ID) {
+        return this.model.findByIdAndUpdate(id, {
+            deleted: true,
+        })
     }
 }
