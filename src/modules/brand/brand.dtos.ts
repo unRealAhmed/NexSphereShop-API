6894871184bd42d96cf22d64b2acd01485c25baa
Ahmed Sayed <ahmed.sayed.connect@gmail.com>
2024-05-06 16:34:28 +0300

    add brand dtos

---
+++ src/modules/brand/brand.dtos.ts
@@ -0,0 +1,8 @@
+import { z } from 'zod'
+import { brandSchemas } from '../../validations'
+
+export type CreateBrandDTO = z.infer<typeof brandSchemas.createBrand.body>
+
+export type UpdateBrandDTO = z.infer<typeof brandSchemas.updateBrand.body>
+
+export type FindBrandByIdDTO = z.infer<typeof brandSchemas.findBrandById.params>
