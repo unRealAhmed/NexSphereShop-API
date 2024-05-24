2f9d430c858c7f76179c8720d83c77d62fc5df22
Ahmed Sayed <ahmed.sayed.connect@gmail.com>
2024-05-24 16:18:00 +0300

    add new method for soft delete

--- src/repositories/category.repository.ts
+++ src/repositories/category.repository.ts
@@ -1,8 +1,16 @@
 import Category, { ICategory } from '../models/category.model'
+import { ID } from '../shared/types'
 import { AbstractRepository } from './abstract.repository'
 
 export class CategoryRepository extends AbstractRepository<ICategory> {
     constructor() {
         super(Category)
+    }
+
+    softDelete(id: ID) {
+        return this.model.findByIdAndUpdate(id, {
+            deletedAt: new Date(),
+            deleted: true,
+        })
     }
 }
