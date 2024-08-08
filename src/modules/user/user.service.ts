65775eec6f0c7d36aa072a01691029fa97192ec0
Ahmed Sayed <ahmed.sayed.connect@gmail.com>
2024-08-08 12:45:05 +0300

    use user error messages

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,5 +1,6 @@
 import { IUser } from '../../models'
 import { UserRepository } from '../../repositories'
+import { ErrorMessages } from '../../shared/constants/errorMessages' // Importing error messages
 import { BadRequestError, NotFoundError } from '../../shared/errors/errors'
 import * as passwordService from '../../shared/helpers/password'
 import { ID } from '../../shared/types'
@@ -24,7 +25,7 @@ export class UserService {
 
         const existingUser = await this.userRepository.findByEmail(email)
         if (existingUser) {
-            throw new BadRequestError('Email is already in use')
+            throw new BadRequestError(ErrorMessages.EMAIL_ALREADY_IN_USE)
         }
 
         const hashedPassword = await passwordService.hashPassword(password)
@@ -45,32 +46,32 @@ export class UserService {
 
     async findById(userId: ID): Promise<IUser> {
         const user = await this.userRepository.findById(userId)
-        if (!user) throw new NotFoundError('User not found')
+        if (!user) throw new NotFoundError(ErrorMessages.USER_NOT_FOUND)
         return user
     }
 
     async updateUser(userId: ID, data: UpdateUserDTO): Promise<IUser> {
         const updatedUser = await this.userRepository.updateById(userId, data)
-        if (!updatedUser) throw new NotFoundError('User not found')
+        if (!updatedUser) throw new NotFoundError(ErrorMessages.USER_NOT_FOUND)
         return updatedUser
     }
 
     async getMe(userId: ID): Promise<IUser> {
         const user = await this.userRepository.findById(userId)
-        if (!user) throw new NotFoundError('User not found')
+        if (!user) throw new NotFoundError(ErrorMessages.USER_NOT_FOUND)
         return user
     }
 
     async deleteUser(userId: ID): Promise<void> {
         const deletedUser = await this.userRepository.deleteById(userId)
-        if (!deletedUser) throw new NotFoundError('User not found')
+        if (!deletedUser) throw new NotFoundError(ErrorMessages.USER_NOT_FOUND)
     }
 
     async assignRole(userId: ID, role: AssignRoleDTO['role']): Promise<IUser> {
         const updatedUser = await this.userRepository.updateById(userId, {
             role,
         })
-        if (!updatedUser) throw new NotFoundError('User not found')
+        if (!updatedUser) throw new NotFoundError(ErrorMessages.USER_NOT_FOUND)
         return updatedUser
     }
 
@@ -79,24 +80,26 @@ export class UserService {
         data: UpdateCurrentUserDTO,
     ): Promise<IUser> {
         const updatedUser = await this.userRepository.updateById(userId, data)
-        if (!updatedUser) throw new BadRequestError('Failed to update user')
+        if (!updatedUser)
+            throw new BadRequestError(ErrorMessages.FAILED_TO_UPDATE_USER)
         return updatedUser
     }
 
     async changePassword(userId: ID, data: ChangePasswordDTO): Promise<IUser> {
         const { oldPassword, newPassword, passwordConfirm } = data
         const user = await this.userRepository.findById(userId)
-        if (!user) throw new NotFoundError('User not found')
+        if (!user) throw new NotFoundError(ErrorMessages.USER_NOT_FOUND)
 
         if (newPassword !== passwordConfirm) {
-            throw new BadRequestError('New passwords must match')
+            throw new BadRequestError(ErrorMessages.NEW_PASSWORDS_MUST_MATCH)
         }
 
         const isPasswordCorrect = passwordService.comparePassword(
             oldPassword,
             user.password,
         )
-        if (!isPasswordCorrect) throw new BadRequestError('Incorrect password')
+        if (!isPasswordCorrect)
+            throw new BadRequestError(ErrorMessages.INCORRECT_PASSWORD)
 
         const hashedPassword = await passwordService.hashPassword(newPassword)
         return this.userRepository.updatePassword(userId, hashedPassword)
@@ -115,7 +118,7 @@ export class UserService {
 
     async findByEmail(email: string): Promise<IUser> {
         const user = await this.userRepository.findByEmail(email)
-        if (!user) throw new NotFoundError('user not found')
+        if (!user) throw new NotFoundError(ErrorMessages.USER_NOT_FOUND)
         return user
     }
 }
