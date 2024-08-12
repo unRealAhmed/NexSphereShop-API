fb8f58d2e3b24a67c16239ce7c4eb4cf7772b2b1
Ahmed Sayed <ahmed.sayed.connect@gmail.com>
2024-08-12 12:44:40 +0300

    add user related messages

--- src/shared/constants/errorMessages.ts
+++ src/shared/constants/errorMessages.ts
@@ -1,5 +1,13 @@
 export const ErrorMessages = {
+    // Product related errors
     BRAND_NOT_FOUND: 'Brand not found',
     CATEGORY_NOT_FOUND: 'Category not found',
     PRODUCT_NOT_FOUND: 'Product not found',
+
+    // User related errors
+    EMAIL_ALREADY_IN_USE: 'Email is already in use',
+    USER_NOT_FOUND: 'User not found',
+    FAILED_TO_UPDATE_USER: 'Failed to update user',
+    NEW_PASSWORDS_MUST_MATCH: 'New passwords must match',
+    INCORRECT_PASSWORD: 'Incorrect password',
 }
