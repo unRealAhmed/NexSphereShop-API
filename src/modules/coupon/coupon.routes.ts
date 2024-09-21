de25d5e0cfac0981bcf5feb626b165f5ac4cf1d7
Ahmed Sayed <ahmed.sayed.connect@gmail.com>
2024-09-21 04:30:28 +0300

    init routes

---
+++ src/modules/coupon/coupon.routes.ts
@@ -0,0 +1,22 @@
+// import { protect, restrictTo } from '@modules/user/auth.Controller'
+// import express from 'express'
+// import {
+//     createCoupon,
+//     deleteCoupon,
+//     getAllCoupons,
+//     getCoupon,
+//     updateCoupon,
+// } from './coupon.Controller'
+
+// const router = express.Router()
+
+// router.get('/', getAllCoupons)
+// router.get('/:id', getCoupon)
+
+// router.use(protect, restrictTo('admin'))
+
+// router.post('/', createCoupon)
+
+// router.route('/:id').patch(updateCoupon).delete(deleteCoupon)
+
+// export default router
