da15847f719d98b5022d80ad3f59f241ec7f7544
Ahmed Sayed <ahmed.sayed.connect@gmail.com>
2024-09-28 02:09:22 +0300

    change name to router

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,40 +1,37 @@
 import express from 'express'
-import { extractUserFromToken } from '../../app/middlewares/auth'
-import { validate } from '../../app/middlewares/validation.middleware'
+import { extractUserFromToken } from '../../middlewares/auth'
+import { validate } from '../../middlewares/validation.middleware'
 import { createRoute } from '../../shared/helpers/routeHelper'
 import { userSchemas } from '../../validations'
 import { UserController } from './user.controller'
 
-const userRouter = express.Router()
+const router = express.Router()
 const userController = new UserController()
 
-userRouter.use(extractUserFromToken)
+router.use(extractUserFromToken)
 
-// Update Current User Profile
 createRoute(
-    userRouter,
+    router,
     'patch',
     '/update',
     validate(userSchemas.update),
     userController.updateCurrentUser.bind(userController),
 )
 
-// Change Password
 createRoute(
-    userRouter,
+    router,
     'patch',
     '/change-password',
     validate(userSchemas.updatePassword),
     userController.changePassword.bind(userController),
 )
 
-// Update Shipping Address
 createRoute(
-    userRouter,
+    router,
     'patch',
     '/update-shipping-address',
     validate(userSchemas.update),
     userController.updateShippingAddress.bind(userController),
 )
 
-export default userRouter
+export default router
