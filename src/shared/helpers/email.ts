a9f45a62183d78104325afba70ca429d7ec48a9a
Ahmed Sayed <ahmed.sayed.connect@gmail.com>
2024-09-29 15:07:32 +0300

    fix forgot password email send

--- src/shared/helpers/email.ts
+++ src/shared/helpers/email.ts
@@ -12,7 +12,6 @@ export class EmailService {
             'NexSphereShop Team <noreply@NexSphereShop.io>'
     }
 
-    // Send email with event emitter
     send(subject: string, to: string, message?: string, html?: string): void {
         const mailOptions = {
             from: this.from,
@@ -22,29 +21,25 @@ export class EmailService {
             html: html || '',
         }
 
-        // Emit the 'sendEmail' event with mailOptions
         emailEventEmitter.emit('sendEmail', mailOptions)
     }
 
-    // Welcome email
     sendWelcomeEmail(to: string, htmlContent: string): void {
         const subject = 'Welcome To NexSphereShop Family 🚀'
 
         this.send(subject, to, undefined, htmlContent)
     }
 
-    // Password reset email
     sendPasswordResetEmail(
         to: string,
         resetURL: string,
         htmlContent: string,
     ): void {
         const subject = 'Password Reset Request'
-        this.send(subject, to, htmlContent)
+        this.send(subject, to, undefined, htmlContent)
     }
 }
 
-// Listen for 'sendEmail' events
 emailEventEmitter.on('sendEmail', async mailOptions => {
     const transporter = nodemailer.createTransport({
         service: 'gmail',
@@ -59,6 +54,5 @@ emailEventEmitter.on('sendEmail', async mailOptions => {
         console.log('Email sent successfully')
     } catch (error) {
         console.error('Error sending email:', error)
-        // Log error for debugging but do not throw to the user
     }
 })
