4c388f3541a60d145cb16b73f054b1e0eea6d873
Ahmed Sayed <ahmed.sayed.connect@gmail.com>
2024-10-04 21:36:23 +0300

    init product routes

--- src/startup/routes.ts
+++ src/startup/routes.ts
@@ -2,12 +2,14 @@ import { Express } from 'express'
 import swaggerDocs from '../config/swagger.config'
 import auth from '../modules/auth/auth.routes'
 import healthCheck from '../modules/healthCheck'
+import product from '../modules/product/product.routes'
 import user from '../modules/user/user.routes'
 
 export default function (app: Express, port: number) {
     app.use('/api/auth', auth)
     app.use('/api/healthCheck', healthCheck)
     app.use('/api/users', user)
+    app.use('/api/products', product)
 
     swaggerDocs(app, port)
 }
