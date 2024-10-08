e6549d4013b86249708c60cae589d762d2934267
Ahmed Sayed <ahmed.sayed.connect@gmail.com>
2024-10-08 21:34:49 +0300

    add create and update dtos

---
+++ src/modules/product/product.dtos.ts
@@ -0,0 +1,12 @@
+import { z } from 'zod'
+import { productSchemas } from '../../validations'
+
+export type CreateProductDTO = z.infer<typeof productSchemas.createProduct.body>
+export type UpdateProductDTO = z.infer<typeof productSchemas.updateProduct.body>
+export type ApplyDiscountDTO = z.infer<typeof productSchemas.applyDiscount.body>
+// export type FindAllProductsDTO = z.infer<
+//     typeof productSchemas.findAllProducts.query
+// >
+// export type FindProductByIdDTO = z.infer<
+//     typeof productSchemas.findProductById.params
+// >
