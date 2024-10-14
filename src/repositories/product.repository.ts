792c26299acb4fac3688f0cf2835bd8aa1751155
Ahmed Sayed <ahmed.sayed.connect@gmail.com>
2024-10-14 19:57:11 +0300

    find lowstock and product by status

--- src/repositories/product.repository.ts
+++ src/repositories/product.repository.ts
@@ -5,4 +5,12 @@ export class ProductRepository extends AbstractRepository<IProduct> {
     constructor() {
         super(Product)
     }
+
+    async findByStatus(status: string): Promise<IProduct[]> {
+        return this.model.find({ status })
+    }
+
+    async findLowStock(threshold: number): Promise<IProduct[]> {
+        return this.model.find({ totalQuantity: { $lt: threshold } })
+    }
 }
