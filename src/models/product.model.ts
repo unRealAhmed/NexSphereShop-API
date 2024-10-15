333e626186f20dfac444bb1b3105395641c10f5e
Ahmed Sayed <ahmed.sayed.connect@gmail.com>
2024-10-15 19:56:43 +0300

    add original price to schema

--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -22,6 +22,7 @@ export interface IProduct extends AbstractDocument {
     user: ID
     images: string[]
     price: number
+    originalPrice: number
     totalQuantity: number
     totalSoldQuantity: number
     discount?: number
@@ -58,6 +59,7 @@ const productSchema = new Schema<IProduct>(
             { type: String, required: true, default: '/uploads/example.jpeg' },
         ],
         price: { type: Number, required: true },
+        originalPrice: { type: Number, required: true },
         totalQuantity: { type: Number, required: true },
         totalSoldQuantity: { type: Number, default: 0 },
         discount: { type: Number, default: 0 },
