db6250a12bdf7f681aeee7794f17c668ba431869
Ahmed Sayed <ahmed.sayed.connect@gmail.com>
2024-10-27 11:18:35 +0300

    add coupon routes

--- src/startup/routes.ts
+++ src/startup/routes.ts
@@ -4,6 +4,7 @@ import auth from '../modules/auth/auth.routes'
 import brand from '../modules/brand/brand.routes'
 import category from '../modules/category/category.routes'
 import color from '../modules/color/color.routes'
+import coupon from '../modules/coupon/coupon.routes'
 import healthCheck from '../modules/healthCheck'
 import product from '../modules/product/product.routes'
 import user from '../modules/user/user.routes'
@@ -16,6 +17,7 @@ export default function (app: Express, port: number) {
     app.use('/api/categories', category)
     app.use('/api/brands', brand)
     app.use('/api/colors', color)
+    app.use('/api/coupons', coupon)
 
     swaggerDocs(app, port)
 }
