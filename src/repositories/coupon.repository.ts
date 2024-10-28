86775f9159caf84e4ab45019f762c093534cce0a
Ahmed Sayed <ahmed.sayed.connect@gmail.com>
2024-10-28 11:17:39 +0300

    add new method for find active coupons

--- src/repositories/coupon.repository.ts
+++ src/repositories/coupon.repository.ts
@@ -5,4 +5,13 @@ export class CouponRepository extends AbstractRepository<ICoupon> {
     constructor() {
         super(Coupon)
     }
+
+    async findByCodeAndActive(code: string): Promise<ICoupon | null> {
+        return this.findOne({
+            code,
+            isActive: true,
+            startDate: { $lte: new Date() },
+            endDate: { $gte: new Date() },
+        })
+    }
 }
