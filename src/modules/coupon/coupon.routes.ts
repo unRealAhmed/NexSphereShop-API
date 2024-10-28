ddb8fac506c95bce2aa3595a0526177f77e41d12
Ahmed Sayed <ahmed.sayed.connect@gmail.com>
2024-10-28 11:18:21 +0300

    update coupon routes

--- src/modules/coupon/coupon.routes.ts
+++ src/modules/coupon/coupon.routes.ts
@@ -1,22 +1,75 @@
-// import { protect, restrictTo } from '@modules/user/auth.Controller'
-// import express from 'express'
-// import {
-//     createCoupon,
-//     deleteCoupon,
-//     getAllCoupons,
-//     getCoupon,
-//     updateCoupon,
-// } from './coupon.Controller'
+import express from 'express'
+import { extractUserFromToken } from '../../middlewares/auth'
+import { validate } from '../../middlewares/validation.middleware'
+import { createRoute } from '../../shared/helpers/routeHelper'
+import { couponSchemas } from '../../validations'
+import { CouponController } from './coupon.Controller'
 
-// const router = express.Router()
+const router = express.Router()
+const couponController = new CouponController()
 
-// router.get('/', getAllCoupons)
-// router.get('/:id', getCoupon)
+// 1. Create Coupon
+createRoute(
+    router,
+    'post',
+    '/',
+    extractUserFromToken,
+    validate(couponSchemas.createCoupon),
+    couponController.createCoupon.bind(couponController),
+)
 
-// router.use(protect, restrictTo('admin'))
+// 2. Get All Coupons
+createRoute(
+    router,
+    'get',
+    '/',
+    couponController.getAllCoupons.bind(couponController),
+)
 
-// router.post('/', createCoupon)
+// 3. Find One Coupon by ID
+createRoute(
+    router,
+    'get',
+    '/:id',
+    validate(couponSchemas.findCouponById),
+    couponController.getCoupon.bind(couponController),
+)
 
-// router.route('/:id').patch(updateCoupon).delete(deleteCoupon)
+// 4. Update Coupon
+createRoute(
+    router,
+    'patch',
+    '/:id',
+    extractUserFromToken,
+    validate(couponSchemas.updateCoupon),
+    couponController.updateCoupon.bind(couponController),
+)
 
-// export default router
+// 5. Delete Coupon
+createRoute(
+    router,
+    'delete',
+    '/:id',
+    extractUserFromToken,
+    couponController.deleteCoupon.bind(couponController),
+)
+
+// 6. Apply Coupon
+createRoute(
+    router,
+    'post',
+    '/apply',
+    validate(couponSchemas.applyCoupon),
+    couponController.applyCoupon.bind(couponController),
+)
+
+// 7. Deactivate Expired Coupons
+createRoute(
+    router,
+    'patch',
+    '/deactivate-expired',
+    extractUserFromToken,
+    couponController.deactivateExpiredCoupons.bind(couponController),
+)
+
+export default router
