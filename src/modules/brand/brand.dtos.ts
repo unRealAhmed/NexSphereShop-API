34fe47c153d10aabda3252d8529616dd3ed2ad75
Ahmed Sayed <ahmed.sayed.connect@gmail.com>
2024-10-28 11:15:05 +0300

    remove find by id dtos

--- src/modules/brand/brand.dtos.ts
+++ src/modules/brand/brand.dtos.ts
@@ -4,5 +4,3 @@ import { brandSchemas } from '../../validations'
 export type CreateBrandDTO = z.infer<typeof brandSchemas.createBrand.body>
 
 export type UpdateBrandDTO = z.infer<typeof brandSchemas.updateBrand.body>
-
-export type FindBrandByIdDTO = z.infer<typeof brandSchemas.findBrandById.params>
