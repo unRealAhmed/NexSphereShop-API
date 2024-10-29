01dd4d62016a2c5f9ed5cda1e6c94bfc50aebe90
Ahmed Sayed <ahmed.sayed.connect@gmail.com>
2024-10-29 11:16:20 +0300

    add some coupon messages

--- src/shared/constants/errorMessages.ts
+++ src/shared/constants/errorMessages.ts
@@ -4,6 +4,7 @@ export const ErrorMessages = {
     CATEGORY_NOT_FOUND: 'Category not found',
     PRODUCT_NOT_FOUND: 'Product not found',
     COLOR_NOT_FOUND: 'color not found',
+    COUPON_NOT_FOUND: 'coupon not found',
 
     // User related errors
     EMAIL_ALREADY_IN_USE: 'Email is already in use',
@@ -27,4 +28,12 @@ export const ErrorMessages = {
 
     // Brand related errors
     BRAND_ALREADY_EXISTS: 'Brand already exists',
+
+    // Coupon related errors
+    COUPON_ALREADY_EXISTS: 'Coupon already exists',
+    COUPON_NOT_FOUND_OR_INACTIVE: 'Coupon not found or inactive',
+    NO_EXPIRED_COUPONS_FOUND: 'No expired coupons found',
+    START_DATE_MUST_BE_IN_FUTURE: 'Start date must be today or in the future.',
+    END_DATE_MUST_BE_AFTER_START_DATE:
+        'End date must be equal to or after the start date.',
 }
