1065e47e620c0397d12b50c562af002d828ecde2
Ahmed Sayed <ahmed.sayed.connect@gmail.com>
2024-10-31 05:30:03 +0300

    update review routes

--- src/modules/review/review.routes.ts
+++ src/modules/review/review.routes.ts
@@ -1,22 +1,57 @@
-// import { protect, restrictTo } from '@modules/user/auth.Controller'
-// import express from 'express'
-// import {
-//     createReview,
-//     // deleteReview,
-//     // getAllReviews,
-//     // getReview,
-//     setReviewUserIds,
-// } from './reviewController'
+import express from 'express'
+import { extractUserFromToken } from '../../middlewares/auth'
+import { validate } from '../../middlewares/validation.middleware'
+import { createRoute } from '../../shared/helpers/routeHelper'
+import { reviewSchemas } from '../../validations'
+import { ReviewController } from './review.controller'
 
-// const reviewRouter = express.Router({ mergeParams: true })
+const router = express.Router()
+const reviewController = new ReviewController()
 
-// reviewRouter.use(protect)
+// 1. Create Review
+createRoute(
+    router,
+    'post',
+    '/:productId',
+    extractUserFromToken,
+    validate(reviewSchemas.createReview),
+    reviewController.createReview.bind(reviewController),
+)
 
-// reviewRouter
-//     .route('/')
-//     // .get(getAllReviews)
-//     .post(restrictTo('user'), setReviewUserIds, createReview)
+// 2. Get All Reviews
+createRoute(
+    router,
+    'get',
+    '/',
+    reviewController.getAllReviews.bind(reviewController),
+)
 
-// // router.route('/:id').get(getReview).patch(updateReview).delete(deleteReview)
+// 3. Find One Review by ID
+createRoute(
+    router,
+    'get',
+    '/:id',
+    validate(reviewSchemas.getReviewById),
+    reviewController.getReview.bind(reviewController),
+)
 
-// export default reviewRouter
+// 4. Update Review
+createRoute(
+    router,
+    'patch',
+    '/:id',
+    extractUserFromToken,
+    validate(reviewSchemas.updateReview),
+    reviewController.updateReview.bind(reviewController),
+)
+
+// 5. Delete Review
+createRoute(
+    router,
+    'delete',
+    '/:id',
+    extractUserFromToken,
+    reviewController.deleteReview.bind(reviewController),
+)
+
+export default router
